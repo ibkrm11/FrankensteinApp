cf7dd8f665266cd24ed35980686e2051632afb21
chenym250 <chenym250@gmail.com>
2014-11-04 04:32:13 -0500

    HELL

--- app/src/main/java/com/example/bikram/frankensteinseries/MyActivity.java
+++ app/src/main/java/com/example/bikram/frankensteinseries/MyActivity.java
@@ -3,28 +3,43 @@ package com.example.bikram.frankensteinseries;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TimePicker;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
 
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity implements AsynResponse {
     private Context context;
-    private boolean searchByTime;
     private String stage;
     private String timeday;
     private String actorName;
     private String eventName;
     private String type;
+    private String result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,14 +111,7 @@ public class MyActivity extends Activity {
             System.out.println("Event Details");
             System.out.println(actorName +" "+ eventName +"  "+ " "+ timeday + "  " +stage   );
 
-            Intent i = new Intent(MyActivity.this, SearchResults.class);
-            i.putExtra(SearchResults.EXTRA_ACTOR, actorName);
-            i.putExtra(SearchResults.EXTRA_EVENT, eventName);
-            i.putExtra(SearchResults.EXTRA_TIMEDAY, timeday);
-            i.putExtra(SearchResults.EXTRA_STAGE, stage);
-            i.putExtra(SearchResults.EXTRA_TYPE, type);
-
-            startActivity(i);
+            postData(type, actorName);
         }
     });
     }
@@ -126,5 +134,108 @@ public class MyActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+
+
+    public String postData(String type, String name) {
+
+        if (type.length() <= 0 || name.length() <= 0) {
+            return null;
+        } else {
+            SenGetReqAsyncTask asyncTask = new SenGetReqAsyncTask(this);
+            asyncTask.execute(type, name);
+        }
+        return "";
+    }
+
+    @Override
+    public void processFinish(String output){
+        result = output;
+        Intent i = new Intent(MyActivity.this, SearchResults.class);
+        i.putExtra(SearchResults.EXTRA_RESULT, result);
+        i.putExtra(SearchResults.EXTRA_TYPE, type);
+
+        startActivity(i);
+    }
+
+    private class SenGetReqAsyncTask extends AsyncTask<String, Void, String> {
+
+        AsynResponse delegate=null;
+
+        public SenGetReqAsyncTask(AsynResponse as){
+            delegate = as;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            delegate.processFinish(result);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String type = params[0];
+            String name = params[1];
+
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet();
+            String hostName = "http://139.147.24.15:8000";
+            String urlName = hostName += "/search/people/?android=true";
+            if (name.length() > 0) {
+                try {
+                    urlName += "&types=[" + type + "]&name=" + URLEncoder.encode(name, "UTF-8").replace("+", "%20");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+            Log.d("url", urlName);
+            try {
+                request.setURI(new URI(urlName));
+                try {
+                    // HttpResponse is an interface just like HttpPost.
+                    // Therefore we can't initialize them
+                    HttpResponse response = client.execute(request);
+
+                    // According to the JAVA API, InputStream constructor do nothing.
+                    //So we can't initialize InputStream although it is not an interface
+                    InputStream inputStream = response.getEntity().getContent();
+
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                    StringBuilder stringBuilder = new StringBuilder();
+
+                    String bufferedStrChunk = null;
+
+                    while ((bufferedStrChunk = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(bufferedStrChunk + '\n');
+                        if (bufferedStrChunk.contains("</html>")) {
+                            throw new RuntimeException("get html file instead of json");
+                        }
+                    }
+                    return stringBuilder.toString();
+                } catch (ClientProtocolException cpe) {
+                    cpe.printStackTrace();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+
+            } catch (URISyntaxException urie) {
+                urie.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
--- app/src/main/java/com/example/bikram/frankensteinseries/SearchResults.java
+++ app/src/main/java/com/example/bikram/frankensteinseries/SearchResults.java
@@ -1,87 +1,49 @@
 package com.example.bikram.frankensteinseries;
 
-import android.app.Activity;
 import android.app.ListActivity;
-import android.app.ActionBar;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URLEncoder;
-import java.nio.charset.Charset;
-import java.nio.charset.IllegalCharsetNameException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Bikram on 11/3/2014.
  */
-public class SearchResults extends ListActivity implements AsynResponse{
+public class SearchResults extends ListActivity{
 
-    public static final String EXTRA_ACTOR = "com.android.bikram.frankensteinseries.actor";
-
-    public static final String EXTRA_EVENT = "com.android.bikram.frankensteinseries.event";
-    public static final String EXTRA_TIMEDAY = "com.android.bikram.frankensteinseries.timeday";
-    public static final String EXTRA_STAGE = "com.android.bikram.frankensteinseries.stage";
     public static final String EXTRA_TYPE = "com.android.bikram.franensteinseries.type";
+    public static final String EXTRA_RESULT = "com.android.bikram.franensteinseries.result";
 
-    private String actor;
-    private String event;
-    private String timeday;
-    private String stage;
     private String type;
-
+    private String result;
+    private String title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.result);
 
-        actor = getIntent().getStringExtra(EXTRA_ACTOR);
-        event  = getIntent().getStringExtra(EXTRA_EVENT);
-        timeday = getIntent().getStringExtra(EXTRA_TIMEDAY);
-        stage = getIntent().getStringExtra(EXTRA_STAGE);
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.result);
+        result = getIntent().getStringExtra(EXTRA_RESULT);
         type = getIntent().getStringExtra(EXTRA_TYPE);
 
-        String result = postData(type, actor);
+        if (type.equals("actor") || type.equals("role")) {
+            title = "  Role   |   Event   |   Stage   |   Time";
+        } else if (type.equals("crew")) {
+            title = "Role   |   Stage";
+        } else {
+            title = "No Result";
+        }
 
-       /* ArrayList<String> results = new ArrayList<String>();
-        results.add(actor);
-        results.add(event);
-        results.add(timeday);
-        results.add(stage);*/
+       ArrayList<String> results;
+        results = parseJson(result);
 
-        /*ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, result);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, results);
         setListAdapter(adapter);
 
-        adapter.notifyDataSetChanged();*/
-
-
-
-
-
-
+        adapter.notifyDataSetChanged();
     }
     /**
      * Creates options menu
@@ -112,116 +74,24 @@ public class SearchResults extends ListActivity implements AsynResponse{
         return super.onOptionsItemSelected(item);
     }
 
-
-    public boolean isConnected(){
-        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected())
-            return true;
-        else
-            return false;
-    }
-
-
-    public String postData(String type, String name) {
-
-        if (type.length() <= 0) {
-            return null;
-        } else {
-            SenGetReqAsyncTask asyncTask = new SenGetReqAsyncTask(this);
-            asyncTask.execute(type, name);
-        }
-        return "";
-    }
-
-    @Override
-    public void processFinish(String output){
-
-        parseJson(output);
-    }
-
-    private class SenGetReqAsyncTask extends AsyncTask<String, Void, String> {
-
-        AsynResponse delegate=null;
-
-        public SenGetReqAsyncTask(AsynResponse as){
-            delegate = as;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            delegate.processFinish(result);
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            String type = params[0];
-            String name = params[1];
-
-            HttpClient client = new DefaultHttpClient();
-            HttpGet request = new HttpGet();
-            String hostName = "http://139.147.24.15:8000";
-            String urlName = hostName += "/search/people/?android=true";
-            if (name.length() > 0) {
-                try {
-                    urlName += "&types=[" + type + "]&name=" + URLEncoder.encode(name, "UTF-8").replace("+", "%20");
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
-            }
-            Log.d("url",urlName);
-            try {
-                request.setURI(new URI(urlName));
-                try {
-                    // HttpResponse is an interface just like HttpPost.
-                    // Therefore we can't initialize them
-                    HttpResponse response = client.execute(request);
-
-                    // According to the JAVA API, InputStream constructor do nothing.
-                    //So we can't initialize InputStream although it is not an interface
-                    InputStream inputStream = response.getEntity().getContent();
-
-                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-
-                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-                    StringBuilder stringBuilder = new StringBuilder();
-
-                    String bufferedStrChunk = null;
-
-                    while ((bufferedStrChunk = bufferedReader.readLine()) != null) {
-                        stringBuilder.append(bufferedStrChunk + '\n');
-                        if (bufferedStrChunk.contains("</html>")) {
-                            throw new RuntimeException("get html file instead of json");
-                        }
-                    }
-                    return stringBuilder.toString();
-                } catch (ClientProtocolException cpe) {
-                    cpe.printStackTrace();
-                } catch (IOException ioe) {
-                    ioe.printStackTrace();
-                }
-
-            } catch (URISyntaxException urie) {
-                urie.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return null;
-        }
-    }
-
     public ArrayList<String> parseJson(String s){
         ArrayList<String> arr = new ArrayList<String>();
-        String[] actor_desc= s.split("]]");
+        arr.add(title);
+        s = s.replace("[[","\n");
+        s = s.replace('"', ' ');
+        s = s.replace('{',' ');
+        s = s.replace('}',' ');
+        s.trim();
+        String[] actor_desc= s.split("]], ");
+
         for(int i=0;i < actor_desc.length; i++){
             //System.out.println(actor_desc[i]);
             actor_desc[i] = actor_desc[i].replace(']','\n');
-            CharSequence cs;
-            actor_desc[i] = actor_desc[i].replace((CharSequence)"[[",(CharSequence)"\n");
+            actor_desc[i] = actor_desc[i].replace(", [","");
+
             System.out.println(actor_desc[i]);
             System.out.println("----------------------------");
+            arr.add( actor_desc[i]);
         }
 
         return arr;
